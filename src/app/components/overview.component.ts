74c6eea47a585765eee16edeb27ea980dbcd96cc
Zuzanna Pacholczyk <zuzannapacholczyk@wp.pl>
2022-03-03 09:12:56 +0100

    initial commit

--- src/app/components/overview.component.ts
+++ src/app/components/overview.component.ts
@@ -1,10 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { HeroesService } from "../services/heroes.service";
 
 @Component({
   selector: "overview",
   templateUrl: "./overview.component.html",
   styleUrls: ["./overview.component.css"]
 })
-export class OverviewComponent {
+export class OverviewComponent implements OnInit {
   title = "Heroes overview";
+  heroes$;
+  constructor(private heroesService: HeroesService, private router: Router) {}
+
+  ngOnInit() {
+    this.heroes$ = this.heroesService.heroesData();
+  }
+
+  editHero(heroName: string) {
+    this.router.navigate(["details", heroName]);
+  }
 }
